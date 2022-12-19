7af9d736d7aa42d2db407c0f2fad484fe6d2675e
SmokedDuck1591 <coutinho159123@gmail.com>
2022-12-19 12:30:26 +0900

    learn array

---
+++ barkingdog/0x03/3273.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1000002];
+
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n, m;
+
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    if (i < n / 2)
+    {
+    }
+  }
+  cin >> m;
+}
--- barkingdog/0x03/note.cpp
+++ barkingdog/0x03/note.cpp
@@ -11,3 +11,10 @@ int main(void)
   for (int i = 0; i < 21; i++)
     fill(b[i], b[i] + 21, 0); // 2차원
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+}
