71dc08b75e5d1ca1f57470b7d08d82786fd2e63d
SmokedDuck1591 <coutinho159123@gmail.com>
2023-01-03 18:10:14 +0900

    backtracking2

---
+++ barkingdog/0x0C/9663.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, ans;
+int board[16][16];
+void func(int k)
+{
+  if (k == n)
+  {
+    ans++;
+    return;
+  }
+}
+
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n;
+}
