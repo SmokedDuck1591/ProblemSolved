ee7a645e3b9bc056cacef72c8c11b7030cbe737c
SmokedDuck1591 <coutinho159123@gmail.com>
2023-01-11 22:42:54 +0900

    recur

---
+++ barkingdog/0x0C/2580.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int board[9][9];
+
+bool isvalid(int x, int y)
+{
+}
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  for (int i = 0; i < 9; i++)
+    for (int j = 0; j < 9; j++)
+      cin >> board[i][j];
+}
--- barkingdog/0x0C/9663.cpp
+++ barkingdog/0x0C/9663.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
 int n, ans;
-int board[16][16];
+bool board[16][16];
+vector<pair<int, int>> v;
 void func(int k)
 {
   if (k == n)
@@ -9,10 +12,27 @@ void func(int k)
     ans++;
     return;
   }
+  for (int i = 0; i < n; i++)
+  {
+    bool flag = true;
+    for (int j = 0; j < v.size(); j++)
+    {
+      if (v[j].Y == i || v[j].X - v[j].Y == k - i || v[j].X + v[j].Y == k + i)
+        flag = false;
+    }
+    if (flag)
+    {
+      v.push_back({k, i});
+      func(k + 1);
+      v.pop_back();
+    }
+  }
 }
 
 int main(void)
 {
   ios::sync_with_stdio(0), cin.tie(0);
   cin >> n;
+  func(0);
+  cout << ans;
 }
