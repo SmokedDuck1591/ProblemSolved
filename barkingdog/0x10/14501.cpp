24abdea3e0e320e244e447f8042ff83a538eaba7
SmokedDuck1591 <coutinho159123@gmail.com>
2023-03-03 22:44:42 +0900

    dp

---
+++ barkingdog/0x10/14501.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int day[17];
+int cost[17];ㅋ
+int dp[17];
+int main(void)
+{
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++)
+    cin >> day[i] >> cost[i];
+
+  for (int i =)
+}
